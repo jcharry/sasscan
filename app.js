d43733a137347b64f2014c3a212b913b47956775
jcharry <jamie.charry@gmail.com>
2016-04-21 13:35:56 +0000

    basic sockets example working

--- app.js
+++ app.js
@@ -2,10 +2,9 @@
 var http = require('http');
 var fs = require('fs'); // Using the filesystem module
 var httpServer = http.createServer(requestHandler);
-httpServer.listen(8080);
 
 var Gpio = require('onoff').Gpio;
-var button = new Gpio(18, 'in', 'both');
+var button = new Gpio(18, 'in', 'falling');
 
 
 
@@ -41,42 +40,15 @@ io.sockets.on('connection', function (socket) {
     button.watch(function(err, value) {
         if (err) console.log(err);
 
+        io.sockets.emit('message', value);
         console.log(value);
 
     });
 
-    //var timer = setInterval(function() {
-        //var state = button.readSync();
-        //if (state === 0) {
-            //io.sockets.emit('message', state);
-        //}
-    //},200);
-	
-		// When this user "send" from clientside javascript, we get a "message"
-		// client side: socket.send("the message");  or socket.emit('message', "the message");
-		//socket.on('message', 
-			//// Run this function when a message is sent
-			//function (data) {
-				//console.log("message: " + data);
-				
-				//// Call "broadcast" to send it to all clients (except sender), this is equal to
-				//// socket.broadcast.emit('message', data);
-				//socket.broadcast.send(data);
-				
-				//// To all clients, on io.sockets instead
-				//// io.sockets.emit('message', "this goes to everyone");
-			//}
-		//);
-		
-		// When this user emits, client side: socket.emit('otherevent',some data);
-		//socket.on('otherevent', function(data) {
-			//// Data comes in as whatever was sent, including objects
-			//console.log("Received: 'otherevent' " + data);
-		//});
-		
-		socket.on('disconnect', function() {
-			console.log("Client has disconnected");
-		});
+    socket.on('disconnect', function() {
+        console.log("Client has disconnected");
+    });
 	}
 );
 
+httpServer.listen(8080);
