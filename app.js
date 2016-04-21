e679318710250f6e94bec12f6aeaa7e8d51b1233
jcharry <jamie.charry@gmail.com>
2016-04-21 08:54:56 -0400

    first commit of sockets branch

--- app.js
+++ app.js
@@ -1,60 +1,74 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// HTTP Portion
+var http = require('http');
+var fs = require('fs'); // Using the filesystem module
+var httpServer = http.createServer(requestHandler);
+httpServer.listen(8080);
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var Gpio = require('onoff').Gpio;
+var button = new Gpio(18, 'in', 'both');
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+// Any normal http request
+function requestHandler(req, res) {
+    //console.log(req);
+	// Read index.html
+	fs.readFile(__dirname + '/index.html', 
+		// Callback function for reading
+		function (err, data) {
+			// if there is an error
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading index.html');
+			}
+			// Otherwise, send the data, the contents of the file
+			res.writeHead(200);
+            res.end(data);
+  		}
+  	);
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io').listen(httpServer);
 
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection', function (socket) {
 
-module.exports = app;
+    console.log("We have a new client: " + socket.id);
+
+    var timer = setInterval(function() {
+        var state = button.readSync();
+        if (state === 0) {
+            io.sockets.emit('message', state);
+        }
+    },200);
+	
+		// When this user "send" from clientside javascript, we get a "message"
+		// client side: socket.send("the message");  or socket.emit('message', "the message");
+		//socket.on('message', 
+			//// Run this function when a message is sent
+			//function (data) {
+				//console.log("message: " + data);
+				
+				//// Call "broadcast" to send it to all clients (except sender), this is equal to
+				//// socket.broadcast.emit('message', data);
+				//socket.broadcast.send(data);
+				
+				//// To all clients, on io.sockets instead
+				//// io.sockets.emit('message', "this goes to everyone");
+			//}
+		//);
+		
+		// When this user emits, client side: socket.emit('otherevent',some data);
+		//socket.on('otherevent', function(data) {
+			//// Data comes in as whatever was sent, including objects
+			//console.log("Received: 'otherevent' " + data);
+		//});
+		
+		socket.on('disconnect', function() {
+			console.log("Client has disconnected");
+		});
+	}
+);
+
